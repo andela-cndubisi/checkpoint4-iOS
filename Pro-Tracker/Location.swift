b5b86d695602fccec65b553009753723be79b6f9
andela-cndubisi <chijioke.ndubisi@andela.co>
2016-06-17 12:00:31 +0100

    Add CoreLocation Extension File

--- Pro-Tracker/Location.swift
+++ Pro-Tracker/Location.swift
@@ -25,33 +25,3 @@ class Location: NSManagedObject {
         self.productivity = (productivity != nil) ? productivity : Productivity(date:date, context: context)
     }
 }
-
-extension CLLocation {
-    func save(_ inContext:NSManagedObjectContext){
-        let geocoder = CLGeocoder()
-        geocoder.reverseGeocodeLocation(self, completionHandler: { (placemark, error) in
-            do {
-                var address = ""
-                
-                if error == nil && placemark?.count > 0 {
-                    if let thoroughfare = placemark!.first!.thoroughfare {
-                        address += thoroughfare + " "
-                    }
-                    if let subAdministrativeArea = placemark!.first!.subAdministrativeArea {
-                        address += subAdministrativeArea + " "
-                    }
-                    if let country = placemark!.first!.country {
-                        address += country + " "
-                    }
-                }else {
-                    address = "Unknown"
-                }
-                let location = Location(lat:self.coordinate.latitude, long:self.coordinate.latitude,address:  address, context:  inContext)
-                try location.managedObjectContext?.save()
-            } catch {
-                print(error)
-            }
-            
-        })
-    }
-}
